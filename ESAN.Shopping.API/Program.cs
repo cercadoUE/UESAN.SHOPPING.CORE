e8372ad66f0fd8e88b8b0158f1fe5cca50e9dea4
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2026-05-07 19:05:06 -0500

    agregado repositories y data

--- ESAN.Shopping.API/Program.cs
+++ ESAN.Shopping.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using UESAN.SHOPPING.CORE.core.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var _config = builder.Configuration;
+var cnx = _config.GetConnectionString("DevConnection");
+builder.Services.AddDbContext<logisticaBDContext>(options => options.UseSqlServer(cnx));
 
 var app = builder.Build();
 
---
+++ UESAN.SHOPPING.CORE/infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UESAN.SHOPPING.CORE.core.Entities;
+
+namespace UESAN.SHOPPING.CORE.infrastructure.Repositories
+{
+    internal class CategoryRepository
+    {
+        public IEnumerable<Category> GetCategories()
+    }
+}
